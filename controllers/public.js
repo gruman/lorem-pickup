c3bb3cc0477db6ced57b4540a8e526e9e96d7a61
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-07-28 04:38:08 -0700

    Update

--- controllers/public.js
+++ controllers/public.js
@@ -9,7 +9,7 @@ function getRandomInt(min, max) {
 
 exports.generate = (req, res, next) => {
   let lines = this.getLines(req, res, req.body.number)
-    lines = this.getNerves(req, res, req.body.number)
+  lines = this.getNerves(req, res, req.body.number)
 };
 
 exports.getNerves = (req, res, number) => {
@@ -18,47 +18,44 @@ exports.getNerves = (req, res, number) => {
     num = number;
   }
   let newWords = "";
-  for (let i = 0; i < num; i++)
-  {
+  for (let i = 0; i < num; i++) {
     newWords += lines[getRandomInt(0, lines.length)] + " ";
-   
-      if (getRandomInt(0, 3) == 1)
-      {
-        let rand = getRandomInt(0, 7);
-        switch (rand) {
-          case 0:
-            newWords += "Er... Uh... ";
-            break;
-          case 1:
-            newWords += "Ummmmmmmm... ";
-            break;
-            case 2:
-              newWords += "OH NO! ";
-              break;
-              case 3:
-                newWords += "OMG! ";
-                break;
-                case 4:
-                  newWords += "I wasn't talking to you! ";
-                  break;
-                  case 5:
-                    newWords += "Is it hot in here?";
-                    break;
-                    case 6:
-                      newWords += "Sorry you were saying something?";
-                      break;
-                      case 7:
-                        newWords += "I lllll... I llooo... I LOVE YOU! AH!";
-                        break;
-        }
+
+    if (getRandomInt(0, 3) == 1) {
+      let rand = getRandomInt(0, 7);
+      switch (rand) {
+        case 0:
+          newWords += "Er... Uh... ";
+          break;
+        case 1:
+          newWords += "Ummmmmmmm... ";
+          break;
+        case 2:
+          newWords += "OH NO! ";
+          break;
+        case 3:
+          newWords += "OMG! ";
+          break;
+        case 4:
+          newWords += "I wasn't talking to you! ";
+          break;
+        case 5:
+          newWords += "Is it hot in here? ";
+          break;
+        case 6:
+          newWords += "Sorry you were saying something? ";
+          break;
+        case 7:
+          newWords += "I lllll... I llooo... I LOVE YOU! AH! ";
+          break;
       }
-    
-    if (i % 4 == 0 && i != 0 && getRandomInt(0, 2) == 1)
-    {
+    }
+
+    if (i % 4 == 0 && i != 0 && getRandomInt(0, 2) == 1) {
       newWords += "\n\n";
     }
   }
-   return(newWords)
+  return (newWords)
 }
 exports.getLines = (req, res, number) => {
   let num = 10;
@@ -66,14 +63,12 @@ exports.getLines = (req, res, number) => {
     num = number
   }
   let newWords = "";
-    for (let i = 0; i < num; i++)
-    {
-      newWords += lines[getRandomInt(0, lines.length)] + " ";
-        
-      if (i % 4 == 0 && i != 0 && getRandomInt(0, 2) == 1)
-      {
-        newWords += "\n\n";
-      }
+  for (let i = 0; i < num; i++) {
+    newWords += lines[getRandomInt(0, lines.length)] + " ";
+
+    if (i % 4 == 0 && i != 0 && getRandomInt(0, 2) == 1) {
+      newWords += "\n\n";
     }
-    return(newWords);
+  }
+  return (newWords);
 }
